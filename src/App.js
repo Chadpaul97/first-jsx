af5e8cbc1c98898e5b24dd2e6ef3241375366736
Chadpaul97 <chadpaul97@gmail.com>
2022-01-12 14:14:17 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello Dojo</h1>
+      </div>
+      <div>
+        <h2>Things i need to do:</h2>
+      </div>
+      <div>
+        <ul className="App-list">
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
+      </div>
     </div>
   );
 }
